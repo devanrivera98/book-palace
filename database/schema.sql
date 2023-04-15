3e7dadd757885df92ccdfe11307b8160931fdf63
Devan Rivera <devanrivera98@gmail.com>
2023-04-15 22:32:20 +0000

    Modified my schema and data sql

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,44 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+
+CREATE TABLE "public"."wishlist" (
+	"wishlist_id" serial NOT NULL,
+	"title" TEXT NOT NULL UNIQUE,
+	"author" TEXT NOT NULL,
+	"isbn" TEXT NOT NULL UNIQUE,
+	"rating" DECIMAL NOT NULL,
+	"image" TEXT NOT NULL UNIQUE,
+	"description" TEXT NOT NULL,
+	"price" DECIMAL NOT NULL UNIQUE,
+	CONSTRAINT "wishlist_pk" PRIMARY KEY ("wishlist_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."cart" (
+	"cartId" serial NOT NULL,
+	"title" TEXT NOT NULL,
+	"author" TEXT NOT NULL,
+	"isbn" TEXT NOT NULL UNIQUE,
+	"rating" DECIMAL NOT NULL,
+	"image" TEXT NOT NULL,
+	"price" TEXT NOT NULL,
+	CONSTRAINT "cart_pk" PRIMARY KEY ("cartId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."users" (
+	"userId" serial NOT NULL,
+	"username" TEXT NOT NULL UNIQUE,
+	"password" TEXT NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
