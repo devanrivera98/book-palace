f721d144d348b5434addcea7d4b799758c0c9810
Devan Rivera <devanrivera98@gmail.com>
2023-04-20 18:13:19 +0000

    added my css again

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,15 @@
-import { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import Header from './Header';
+import Homepage from './Homepage';
 import './App.css';
 
 function App() {
-  const [serverData, setServerData] = useState("");
-
-  useEffect(() => {
-    async function getServerData() {
-      const resp = await fetch('/api/hello');
-      const data = await resp.json();
-
-      console.log('Data from server:', data);
-
-      setServerData(data.message);
-    }
-
-    getServerData();
-  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>{serverData}</h1>
-      </header>
-    </div>
-  );
-}
+    <>
+    <Header/>
+    <Homepage/>
+    </>
+  )
+};
 
 export default App;
