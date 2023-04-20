1e89257e12d9b857105b8fa7d629f5c0b92bb161
Devan Rivera <devanrivera98@gmail.com>
2023-04-20 05:48:49 +0000

    Completed the search bar to make a fetch request

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,9 +1,31 @@
 import './App.css';
+import {useState} from 'react'
 import {BsFillCartFill} from 'react-icons/bs'
 import { BsFillSuitHeartFill } from 'react-icons/bs'
 import { ImSearch } from 'react-icons/im'
 
 export default function Header() {
+  const [search, setSearch] = useState('')
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyCQ7EBy8yMIYuOdh350WfDgEwWKGJSWAh4`)
+      const jsonData= await response.json();
+      if (response.ok) {
+        console.log(response)
+        return console.log('My book requests', jsonData);
+        //placeholder until results page
+      }
+    }
+    catch (error) {
+      console.error('Error fetching image data', error)
+    }
+  }
 
   return (
     <nav className="navbar" style={{ backgroundColor: '#617143'}}>
@@ -14,8 +36,8 @@ export default function Header() {
       </div>
       <div className="container-fluid justify-content-center">
         <div className='justify-content-center col-10' style={{ backgroundColor: '#EDE9D5'}}>
-          <form className="d-flex justify-content-center">
-            <input className="search-bar col-10" placeholder="Search by Title" />
+          <form onSubmit={handleSubmit} className="d-flex justify-content-center">
+            <input className="search-bar col-10" placeholder="Search by Title" onChange={handleChange} value={search} />
             <button className="btn btn-outline-dark"><ImSearch/></button>
           </form>
         </div>
