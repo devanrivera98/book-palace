084bcdcfe3b9679443484c13c91c0db3dfdfd4de
Devan Rivera <devanrivera98@gmail.com>
2023-04-21 23:46:22 +0000

    skeleton placeholder

---
+++ client/src/MoreInfoPage.js
@@ -0,0 +1,24 @@
+export default function MoreInfoPage() {
+
+  return (
+    <div className="container">
+      <div>
+        <h2>Title</h2>
+        <h4>By August Wilson</h4>
+        <p>Average Review: 4.5</p>
+      </div>
+      <div>
+        <img alt="Fences" src="http://books.google.com/books/content?id=HE-iDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" />
+        <p>Add a icon here</p>
+      </div>
+      <div>
+        <h3>Retail Price $20.99</h3>
+        <button></button>
+      </div>
+      <div>
+        <h1>Overview</h1>
+        <p>There should be some information placed here about the book but for now here is just some empty text until I can implement some details in here.</p>
+      </div>
+    </div>
+  )
+}
