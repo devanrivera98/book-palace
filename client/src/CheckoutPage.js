35f80fe83d41d71a17a84e88718f58b34341bd82
Devan Rivera <devanrivera98@gmail.com>
2023-04-27 05:09:22 +0000

    fixed an issue with checkoutpage

--- client/src/CheckoutPage.js
+++ client/src/CheckoutPage.js
@@ -7,7 +7,7 @@ const url = (path) => `${path}`
 
 export default function CheckoutCart() {
   const [cart, setCart] = useState([]);
-
+  const [subtotal, setSubtotal] = useState(0);
   useEffect(() => {
     getRequest();
   }, [])
@@ -20,7 +20,11 @@ export default function CheckoutCart() {
       }
       const jsonData = await response.json()
       setCart(jsonData)
-
+      let newTotal = 0
+      for (let i = 0; i < jsonData.length; i++) {
+        newTotal += Number(jsonData[i].price);
+      }
+      setSubtotal(newTotal.toFixed(2));
     }
     catch (error) {
       console.log(`There was a get error: ${error.message} `)
@@ -52,6 +56,7 @@ export default function CheckoutCart() {
         </div>
         <div className="cartList col-lg-10">
           <h2>YOUR BAG</h2>
+          <h3>Total Items: [{cart.length}]</h3>
         </div>
         <div className="pt-2 text-center">
           <h4>Items in your bag are not reserved - check out now to make them yours</h4>
@@ -62,8 +67,8 @@ export default function CheckoutCart() {
         <div className="cartList col-lg-10">
           <h3 className="pt-3 text-center">Order Summary</h3>
           <div className="py-2 d-flex justify-content-between">
-            <h3> Number of items: </h3>
-            <h4>Price Holder</h4>
+            <h3> Subtotal</h3>
+            <h4>${subtotal}</h4>
           </div>
           <div className="py-2 d-flex justify-content-between">
             <h3>Delivery</h3>
