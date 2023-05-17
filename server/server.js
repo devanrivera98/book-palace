fed045df917d86dd45dbd1c89996fe8ab12f6b84
Devan Rivera <devanrivera98@gmail.com>
2023-05-17 21:40:50 +0000

    server configuration for client-side-routing

--- server/server.js
+++ server/server.js
@@ -143,6 +143,8 @@ app.delete('/api/wishlist/:wishlistId', async (req, res) => {
   }
 });
 
+app.get('*', (req, res) => res.sendFile(`${reactStaticDir}/index.html`));
+
 app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
