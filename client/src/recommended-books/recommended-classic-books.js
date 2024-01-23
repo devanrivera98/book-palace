b81eab6ba6a16f702b726e75bb82f0e11c93ad23
Devan Rivera <devanrivera98@gmail.com>
2024-01-23 21:43:52 +0000

    set up second recommendation books file

---
+++ client/src/recommended-books/recommended-classic-books.js
@@ -0,0 +1,35 @@
+const recommendationClassics = [
+{
+    title: 'Fences', isbn: '9780593087589', image: 'http://books.google.com/books/content?id=HE-iDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+},
+{
+  title: 'The Great Gatsby', isbn: '9780143136125', image: 'http://books.google.com/books/content?id=Al4NEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+},
+{
+  title: 'The Outsider', isbn: '9781501180989', image: 'http://books.google.com/books/content?id=yK_iyQEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+},
+{
+  title: 'Invisible Man', isbn: '9780307743992', image: 'http://books.google.com/books/content?id=iSrI-BQqFf0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+},
+{
+  title: 'Beloved', isbn: '9781400033416', image: 'http://books.google.com/books/content?id=bm-KDQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+},
+{
+  title: 'Little Women', isbn: '9780140390698', image: 'http://books.google.com/books/content?id=MO3SEL6qIsgC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+},
+{
+  title: 'On the Road: the Original Scroll', isbn: '9780143105466', image: 'http://books.google.com/books/content?id=DaKMEAAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+},
+{
+  title: 'Pride and Prejudice', author:'Jane Austen', isbn: '9780521825146', image: 'http://books.google.com/books/content?id=yxIHAemJKM4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', price: 19.99
+},
+{
+  title: '1984', author: 'George Orwell', isbn: '9780547249643', image: 'http://books.google.com/books/content?id=kotPYEqx7kMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', price: 13.99
+},
+{
+  title: 'Atonement', author: 'Ian McEwan', isbn: '9781400075553', image: 'http://books.google.com/books/content?id=pX6lBs7Q0MEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', price: 12.99
+},
+{
+  title: 'White Teeth', author: 'Zadie Smith', isbn: '9781400075508', image: 'http://books.google.com/books/content?id=zZFlmid0HOYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', price: 12.99
+},
+];
