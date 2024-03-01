28fac0a711ad9348669c7b4173e58a6237b238cc
Devan Rivera <devanrivera98@gmail.com>
2024-03-01 03:43:31 +0000

    created spaces when using credit card digits

--- client/src/components/CheckoutPaymentInfo.js
+++ client/src/components/CheckoutPaymentInfo.js
@@ -21,8 +21,9 @@ export default function CheckoutPaymentInfo({onShow, isActive, updatePaymentInfo
 
   }, [cardNumber, expirationDate, cvv]);
 
-  function handleCreditCard(e) {
-    const formattedValue = e.target.value.replace(/(.{4})/g, '$1 ').trim();
+  function handleCreditCard(value) {
+    const cleanedValue = value.replace(/\D/g, '');
+    const formattedValue = cleanedValue.replace(/(.{4})/g, '$1 ').trim();
     updatePaymentInfo({ cardNumber: formattedValue })
   }
 
@@ -47,7 +48,7 @@ export default function CheckoutPaymentInfo({onShow, isActive, updatePaymentInfo
       <div className="border d-flex flex-column my-4">
         <div className="payment-box-container d-flex flex-column align-items-center mx-auto">
           <div className="w-100 py-3">
-            <input className="w-100 py-2" type="text" inputMode="numeric" pattern="[0-9]*" maxLength={19} placeholder="Card Number" required onChange={(e) => updatePaymentInfo({cardNumber: e.target.value})} value={cardNumber} onKeyDown={(e) => handleNumbericInputs(e)}
+            <input className="w-100 py-2" type="text" inputMode="numeric" pattern="[0-9]*" maxLength={19} placeholder="Card Number" required onChange={(e) => handleCreditCard(e.target.value)} value={cardNumber} onKeyDown={(e) => handleNumbericInputs(e)}
               />
           </div>
           <div className="w-100 py-3">
@@ -59,7 +60,7 @@ export default function CheckoutPaymentInfo({onShow, isActive, updatePaymentInfo
         </div>
       </div>
       <div className="d-flex">
-        <button className={`mx-auto checkout-button ${isFormValid ? 'dark-checkout-button' : 'light-checkout-button'}`}>Continue to Order Review</button>
+        <button type="submit" className={`mx-auto checkout-button ${isFormValid ? 'dark-checkout-button' : 'light-checkout-button'}`}>Continue to Order Review</button>
       </div>
     </div>
     :
