87daf114f9aa999b0ea8ec9c7443c3dda506a2e8
Devan Rivera <devanrivera98@gmail.com>
2024-03-19 22:53:40 +0000

    fixed width of items in order review

--- client/src/components/CheckoutSideMobile.js
+++ client/src/components/CheckoutSideMobile.js
@@ -2,7 +2,9 @@ export default function CheckoutSideMobile({ subtotal, total, estimatedDay, esti
 
   const itemsMap = items.map((item) =>
     <div key={item.isbn} className="d-flex d-md-none align-items-center px-1 py-2">
-      <img className="w-25" src={item.image} alt={item.title} />
+      <div style={{ width: 100, height: 150 }}>
+        <img className="w-100 h-100" src={item.image}></img>
+      </div>
       <div className="px-4">
         <h5>{item.title}</h5>
         <span>Qty: {item.quantity} @ ${item.price}</span>
@@ -20,7 +22,7 @@ export default function CheckoutSideMobile({ subtotal, total, estimatedDay, esti
           </div>
           {itemsMap}
           <div>
-            <h5 className="m-0 py-2 text-center">By clicking the "Submit Payment" button you confirm that you did not input any real information besides a email that will be used to send real confirmation</h5>
+            <h5 className="m-0 py-2 px-5 text-center">By clicking the "Submit Order" button you confirm that you did not input any real information besides a email that will be used to send real confirmation.</h5>
           </div>
           <div className="text-center pb-2">
             <button type="submit" className='checkout-button dark-checkout-button px-5' >Submit Order</button>
