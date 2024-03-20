67b10c45e149b67b179a9016977aee7ebd2394ea
Devan Rivera <devanrivera98@gmail.com>
2024-03-20 03:01:50 +0000

    model for notice is fully created

--- client/src/components/CheckoutModal.js
+++ client/src/components/CheckoutModal.js
@@ -3,9 +3,9 @@ export default function CheckoutModal() {
     <div className="checkout-modal">
       <div className="checkout-modal-content d-flex justify-content-between flex-column">
         <h2 className="text-center">Important Notice</h2>
-        <p style={{ fontSize: '25px' }} className='text-center'>Thank you for trying out Book Palace. Please make sure to not include any personal information besides a email if you wish to receive an email confirmation of your order.</p>
+        <p style={{ fontSize: '25px' }} className='text-center'>Thank you for trying out Book Palace. Please make sure to not include any personal information besides an email if you wish to receive an email confirmation of your mock order.</p>
         <div className="d-flex justify-content-center align-content-end">
-          <button className='checkout-button dark-checkout-button px-md-5 px-4'>I understand</button>
+          <button className='checkout-modal-button dark-checkout-button'>I understand</button>
         </div>
       </div>
     </div>
