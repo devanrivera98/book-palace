09253dd5ab38b7bb217bc2e697df7231cda29f9c
Devan Rivera <devanrivera98@gmail.com>
2024-03-22 22:40:10 +0000

    changed the dimensions of the popup modal

--- client/src/components/CheckoutModal.js
+++ client/src/components/CheckoutModal.js
@@ -23,7 +23,7 @@ export default function CheckoutModal() {
   return (
     <div className={`checkout-modal ${isModalOpen ? '' : 'd-none'}`}>
       <div className="checkout-modal-content d-flex justify-content-between flex-column">
-        <h2 className="text-center">Important Notice</h2>
+        <h2 className="text-center m-0" style={{fontWeight: '700'}}>Important Notice</h2>
         <p style={{ fontSize: '25px' }} className='text-center'>Thank you for trying out Book Palace. Please make sure to not include any personal information besides an email if you wish to receive an email confirmation of your mock order.</p>
         <div className="d-flex justify-content-center align-content-end">
           <button className='checkout-modal-button dark-checkout-button' onClick={closeModal}>I understand</button>
