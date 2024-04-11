70c7bae59c8e50d237d63a97d86092c206c06e78
Devan Rivera <devanrivera98@gmail.com>
2024-04-11 03:14:14 +0000

    successfully layed out the carousel to rotate

--- client/src/components/HomepageCarousel.js
+++ client/src/components/HomepageCarousel.js
@@ -1,28 +1,29 @@
-export default function HomepageCarousel() {
+import { useState, useEffect } from "react";
 
+export default function HomepageCarousel() {
   const carouselImages = [{ name: 'carousel-image-three', desktopImage: 'images/homepage-slider-three.png', mobileImage: 'images/homepage-slider-three-mini.png' }, { name: 'carousel-image-two', desktopImage: 'images/homepage-slider-two.png', mobileImage: 'images/homepage-slider-two-mini.png' }, { name: 'carousel-image-one', desktopImage: 'images/homepage-slider-one.png', mobileImage: 'images/homepage-slider-one-mini.png' }];
+
+  const [carouselCount, setCarouselCount] = useState(0)
+
+  useEffect(() => {
+
+    function IncreaseCount() {
+      setCarouselCount(((carouselCount + 1) % carouselImages.length));
+    }
+
+    const interval = setInterval(IncreaseCount, 3500)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [carouselCount])
 
  return (
   <>
     <picture>
-      <source srcSet={carouselImages[0].mobileImage} media="(max-width: 761px)"></source>
-       <source srcSet={carouselImages[0].desktopImage} media="(max-width: 1200px)"></source>
-      <img src={carouselImages[0].desktopImage} />
+      <source srcSet={carouselImages[carouselCount].mobileImage} media="(max-width: 761px)"></source>
+       <source srcSet={carouselImages[carouselCount].desktopImage} media="(max-width: 1200px)"></source>
+      <img src={carouselImages[carouselCount].desktopImage} />
     </picture>
   </>
  )
 }
-
-
-// function HomepageScroller() {
-
-//   return (
-//     <>
-//       <picture>
-//         <source srcSet="images/homepage-slider-one-mini.png" media="(max-width: 761px)"></source>
-//         <source srcSet="images/homepage-slider-one.png" media="(max-width: 1200px)"></source>
-//         <img src="images/homepage-slider-one.png" />
-//       </picture>
-//     </>
-//   )
-// }
