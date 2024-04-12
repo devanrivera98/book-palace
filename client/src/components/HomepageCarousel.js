86ae1734e7f50940702e8ba5903164436ebead6a
Devan Rivera <devanrivera98@gmail.com>
2024-04-12 00:47:11 +0000

    carousel can now be successfully clicked and navigates based on what view is shown

--- client/src/components/HomepageCarousel.js
+++ client/src/components/HomepageCarousel.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function HomepageCarousel() {
-  const carouselImages = [{ name: 'carousel-image-three', desktopImage: 'images/homepage-slider-three.png', mobileImage: 'images/homepage-slider-three-mini.png', list: awardBooks, navType: '/result' }, { name: 'carousel-image-two', desktopImage: 'images/homepage-slider-two.png', mobileImage: 'images/homepage-slider-two-mini.png', list: ourFavoriteBooks, navType: '/result' }, { name: 'carousel-image-one', desktopImage: 'images/homepage-slider-one.png', mobileImage: 'images/homepage-slider-one-mini.png', list: cheerfullyRefuseBook, navType: '/info' }];
+  const carouselImages = [{ name: 'carousel-image-three', desktopImage: 'images/homepage-slider-three.png', mobileImage: 'images/homepage-slider-three-mini.png', list: awardBooks, navType: '/results' }, { name: 'carousel-image-two', desktopImage: 'images/homepage-slider-two.png', mobileImage: 'images/homepage-slider-two-mini.png', list: ourFavoriteBooks, navType: '/results' }, { name: 'carousel-image-one', desktopImage: 'images/homepage-slider-one.png', mobileImage: 'images/homepage-slider-one-mini.png', list: cheerfullyRefuseBook, navType: '/info' }];
 
   const navigate = useNavigate();
   const [carouselCount, setCarouselCount] = useState(0)
@@ -17,20 +17,22 @@ export default function HomepageCarousel() {
     }
 
 
-    console.log(carouselCount)
-
     const interval = setInterval(IncreaseCount, 3500)
     return () => {
       clearInterval(interval)
     }
   }, [carouselCount])
 
+  function viewBooks(navType, list) {
+    navigate(navType, { state: list })
+  }
+
  return (
   <>
     <picture className="pointer-finger">
       <source srcSet={carouselImages[carouselCount].mobileImage} media="(max-width: 761px)"></source>
        <source srcSet={carouselImages[carouselCount].desktopImage} media="(max-width: 1200px)"></source>
-      <img src={carouselImages[carouselCount].desktopImage} />
+      <img src={carouselImages[carouselCount].desktopImage} onClick={() => viewBooks(carouselImages[carouselCount].navType, carouselImages[carouselCount].list)} />
     </picture>
   </>
  )
