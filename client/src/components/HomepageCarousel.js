e360f0176f39b4d1d880a8541a7183448bf0bf4e
Devan Rivera <devanrivera98@gmail.com>
2024-04-12 00:15:51 +0000

    updated my carousel array to include where each slide will navigate to

--- client/src/components/HomepageCarousel.js
+++ client/src/components/HomepageCarousel.js
@@ -1,8 +1,13 @@
+import { cheerfullyRefuseBook } from "../recommended-books/cheerfully-refuse";
+import { awardBooks } from "../recommended-books/national-award-books";
+import { ourFavoriteBooks } from "../recommended-books/our-favorites-books";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function HomepageCarousel() {
-  const carouselImages = [{ name: 'carousel-image-three', desktopImage: 'images/homepage-slider-three.png', mobileImage: 'images/homepage-slider-three-mini.png' }, { name: 'carousel-image-two', desktopImage: 'images/homepage-slider-two.png', mobileImage: 'images/homepage-slider-two-mini.png' }, { name: 'carousel-image-one', desktopImage: 'images/homepage-slider-one.png', mobileImage: 'images/homepage-slider-one-mini.png' }];
+  const carouselImages = [{ name: 'carousel-image-three', desktopImage: 'images/homepage-slider-three.png', mobileImage: 'images/homepage-slider-three-mini.png', list: awardBooks, navType: '/result' }, { name: 'carousel-image-two', desktopImage: 'images/homepage-slider-two.png', mobileImage: 'images/homepage-slider-two-mini.png', list: ourFavoriteBooks, navType: '/result' }, { name: 'carousel-image-one', desktopImage: 'images/homepage-slider-one.png', mobileImage: 'images/homepage-slider-one-mini.png', list: cheerfullyRefuseBook, navType: '/info' }];
 
+  const navigate = useNavigate();
   const [carouselCount, setCarouselCount] = useState(0)
 
   useEffect(() => {
@@ -10,6 +15,9 @@ export default function HomepageCarousel() {
     function IncreaseCount() {
       setCarouselCount(((carouselCount + 1) % carouselImages.length));
     }
+
+
+    console.log(carouselCount)
 
     const interval = setInterval(IncreaseCount, 3500)
     return () => {
@@ -19,7 +27,7 @@ export default function HomepageCarousel() {
 
  return (
   <>
-    <picture>
+    <picture className="pointer-finger">
       <source srcSet={carouselImages[carouselCount].mobileImage} media="(max-width: 761px)"></source>
        <source srcSet={carouselImages[carouselCount].desktopImage} media="(max-width: 1200px)"></source>
       <img src={carouselImages[carouselCount].desktopImage} />
