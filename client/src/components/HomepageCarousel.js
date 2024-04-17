da6b8713a0030351e1a0007e5b6a3fe953e288bf
Devan Rivera <devanrivera98@gmail.com>
2024-04-17 22:50:23 +0000

    setting up buttons for carousel

--- client/src/components/HomepageCarousel.js
+++ client/src/components/HomepageCarousel.js
@@ -3,6 +3,8 @@ import { awardBooks } from "../recommended-books/national-award-books";
 import { ourFavoriteBooks } from "../recommended-books/our-favorites-books";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { GoDot } from "react-icons/go";
+
 
 export default function HomepageCarousel() {
   const carouselImages = [{ name: 'carousel-image-three', desktopImage: 'images/homepage-slider-three.png', mobileImage: 'images/homepage-slider-three-mini.png', list: awardBooks, navType: '/results' }, { name: 'carousel-image-two', desktopImage: 'images/homepage-slider-two.png', mobileImage: 'images/homepage-slider-two-mini.png', list: ourFavoriteBooks, navType: '/results' }, { name: 'carousel-image-one', desktopImage: 'images/homepage-slider-one.png', mobileImage: 'images/homepage-slider-one-mini.png', list: cheerfullyRefuseBook, navType: '/info' }];
@@ -34,6 +36,11 @@ export default function HomepageCarousel() {
        <source srcSet={carouselImages[carouselCount].desktopImage} media="(max-width: 1200px)"></source>
       <img src={carouselImages[carouselCount].desktopImage} onClick={() => viewBooks(carouselImages[carouselCount].navType, carouselImages[carouselCount].list)} />
     </picture>
+    <div className="mx-auto">
+       <GoDot style={{fontSize: '35px', margin: '0 10px'}} />
+       <GoDot style={{ fontSize: '35px', margin: '0 10px' }} />
+       <GoDot style={{ fontSize: '35px', margin: '0 10px' }} />
+    </div>
   </>
  )
 }
