4e1e196a7ec22b320dd6235fb189dbc09fc5a840
Devan Rivera <devanrivera98@gmail.com>
2025-08-12 11:51:19 -0700

    updated api url

--- client/src/api-url.js
+++ client/src/api-url.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'http://book-palace-server-dev.us-west-1.elasticbeanstalk.com';
+export const API_BASE_URL = 'https://book-palace-server-dev.us-west-1.elasticbeanstalk.com';
