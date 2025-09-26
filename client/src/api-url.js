853f4f514d85fd74ed3b67c93a7ab11bc266b638
Devan Rivera <devanrivera98@gmail.com>
2025-09-25 20:01:02 -0700

    added bootstrap

--- client/src/api-url.js
+++ client/src/api-url.js
@@ -1,5 +1,3 @@
-export const API_BASE_URL = 'https://book-palace-server.onrender.com/';
+export const API_BASE_URL = 'https://api.bookpalaceusa.com';
 
-// export const API_BASE_URL = 'https://api.bookpalaceusa.com';
 // export const API_BASE_URL = 'https://book-palace-server-dev.us-west-1.elasticbeanstalk.com';
-
