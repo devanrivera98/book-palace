51eb74acc3dabefa9d2c1525a441da12cfa2ccc0
Devan Rivera <devanrivera98@gmail.com>
2025-09-25 18:08:37 -0700

    Revert "changed api base"
    This reverts commit 3e0b9cc4c40f59d526994e5af9837df9d73f4aa6.

--- client/src/api-url.js
+++ client/src/api-url.js
@@ -1,3 +1,1 @@
-export const API_BASE_URL = 'https://api.bookpalaceusa.com';
-
-// export const API_BASE_URL = 'https://book-palace-server-dev.us-west-1.elasticbeanstalk.com';
+export const API_BASE_URL = 'https://book-palace-server-dev.us-west-1.elasticbeanstalk.com';
